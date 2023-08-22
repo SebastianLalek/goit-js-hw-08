9ea6c91f347bfb33af5b44bb36676a074ba8487e
Sebastian Lalek <123250026+SebastianLalek@users.noreply.github.com>
2023-08-22 15:01:44 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryImages = galleryItems
+  .map(
+    img =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+  <img
+  class="gallery__image"
+  src="${img.preview}"
+  alt="${img.description}"
+  />
+  </a>
+  </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryImages);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+});
